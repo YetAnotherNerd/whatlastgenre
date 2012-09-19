5b224aadfbde2434de7c27fc782072d613796983
YetAnotherNerd <qpdb@foosion.de>
2012-09-19 02:58:12 +0200

    Initial COmmit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='whatlastgenre',
+      version='0.1',
+      description='Python Distribution Utilities',
+      author='YetAnotherNerd',
+      author_email='qpdb@foosion.de',
+      url='http://github.com/YetAnotherNerd/whatlastgenre',
+      py_modules=['whatlastgenre'],
+     )
