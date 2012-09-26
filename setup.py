c2dbc981edb4d70382133e304e1e081137156de9
YetAnotherNerd <qpdb@foosion.de>
2012-09-26 10:59:13 +0200

    added classifiers

--- setup.py
+++ setup.py
@@ -2,12 +2,24 @@
 
 from setuptools import setup
 
-setup(name='whatlastgenre',
-      version='0.1',
-      description='Improves genre metadata of audio files based on tags from various music-sites.',
-      author='YetAnotherNerd',
-      author_email='qpdb@foosion.de',
-      url='http://github.com/YetAnotherNerd/whatlastgenre',
-      scripts=['whatlastgenre.py'],
-      install_requires=['musicbrainzngs', 'mutagen', 'requests']
-     )
+setup(
+    name='whatlastgenre',
+    version='0.1',
+    description=('Improves genre metadata of audio files based on '
+                 'tags from various music-sites.'),
+    author='YetAnotherNerd',
+    author_email='qpdb@foosion.de',
+    url='http://github.com/YetAnotherNerd/whatlastgenre',
+    scripts=['whatlastgenre.py'],
+    install_requires=[
+        'musicbrainzngs',
+        'mutagen',
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Topic :: Multimedia :: Sound/Audio',
+        "Topic :: Utilities",
+    ],
+)
