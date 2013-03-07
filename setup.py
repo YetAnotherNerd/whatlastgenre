851b1b2307e71691b68b323489f8c7241e666e43
YetAnotherNerd <qpdb@foosion.de>
2013-03-07 15:36:19 +0100

    added tags.txt to package_data

--- setup.py
+++ setup.py
@@ -5,23 +5,18 @@ from setuptools import setup
 setup(
     name='whatlastgenre',
     version='0.1',
-    description=('Improves genre metadata of audio files based on '
-                 'tags from various music-sites.'),
-    author='YetAnotherNerd',
-    author_email='qpdb@foosion.de',
+    description=('Improves genre metadata of audio files '
+                 'based on tags from various music sites.'),
     url='http://github.com/YetAnotherNerd/whatlastgenre',
     # saying tags.txt is a script is ugly but works
     # please tell me a better solution if there is one
     scripts=['whatlastgenre.py', 'tags.txt'],
-    install_requires=[
-        'musicbrainzngs',
-        'mutagen',
-        'requests',
-    ],
+    package_data={'': ['tags.txt']},
+    install_requires=['musicbrainzngs', 'mutagen', 'requests'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Topic :: Multimedia :: Sound/Audio',
         "Topic :: Utilities",
-    ],
+    ]
 )
