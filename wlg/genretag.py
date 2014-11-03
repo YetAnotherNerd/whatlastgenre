a819da9aa7179c35832638c3f325fb9091f776db
YetAnotherNerd <qpdb@foosion.de>
2014-11-03 20:10:58 +0100

    add license

--- wlg/genretag.py
+++ wlg/genretag.py
@@ -1,4 +1,18 @@
-#!/usr/bin/env python
+# whatlastgenre
+# Improve genre metadata of audio files based on tags from various music sites.
+#
+# Copyright (c) 2012-2014 YetAnotherNerd
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
 '''whatlastgenre genretag'''
 
 from __future__ import division, print_function
