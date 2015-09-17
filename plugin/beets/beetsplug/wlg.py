48216f9278cd21c7877ca37d50906a8085577148
YetAnotherNerd <qpdb@foosion.de>
2015-09-17 11:24:59 +0200

    beetsplug: update to use latest code
    also some fixes/improvements

--- plugin/beets/beetsplug/wlg.py
+++ plugin/beets/beetsplug/wlg.py
@@ -17,7 +17,7 @@
 
 '''whatlastgenre beets plugin'''
 
-from __future__ import absolute_import, division, print_function
+from __future__ import absolute_import
 
 from argparse import Namespace
 
@@ -27,11 +27,11 @@ from beets.ui import Subcommand, decargs
 from beetsplug.lastgenre import WHITELIST as BEET_LG_WHITELIST
 
 from wlg import whatlastgenre
-from wlg.whatlastgenre import print_progressbar
+from wlg.mediafile import Metadata
 
 
 class WhatLastGenre(BeetsPlugin):
-    '''First version of the whatlastgenre plugin for beets.'''
+    '''whatlastgenre plugin for beets.'''
 
     def __init__(self):
         super(WhatLastGenre, self).__init__()
@@ -44,88 +44,108 @@ class WhatLastGenre(BeetsPlugin):
         })
         if self.config['auto'].get(bool):
             self.import_stages = [self.imported]
+            self.register_listener('import', self.setdown)
         self.wlg = None
 
-    def lazy_setup(self, cache=False, verbose=0):
-        self.wlg = whatlastgenre.WhatLastGenre(Namespace(
-            tag_limit=self.config['count'].get(int),
-            update_cache=cache, verbose=verbose,
-            interactive=False, dry=False, difflib=False, tag_release=False))
+    def setup(self, update_cache=False, verbose=0):
+        '''Set up the WhatLastGenre object.'''
         whitelist = self.config['whitelist'].get()
-        if not whitelist:
-            whitelist = 'wlg'
-        if whitelist != 'wlg':
-            if whitelist == 'beets':
-                whitelist = BEET_LG_WHITELIST
-            self.wlg.read_whitelist(whitelist)
-        self._log.debug(u'use {0} whitelist with {1} entries.',
-                        whitelist, len(self.wlg.whitelist))
+        if whitelist == 'wlg':
+            whitelist = None
+        elif whitelist == 'beets':
+            whitelist = BEET_LG_WHITELIST
+
+        self.wlg = whatlastgenre.WhatLastGenre(
+            Namespace(update_cache=update_cache, verbose=verbose, dry=False,
+                      tag_limit=self.config['count'].get(int), difflib=False,
+                      tag_release=False, interactive=False), whitelist)
+
+    def setdown(self):
+        '''Since __del__s don't get called we need to do some stuff
+        manually.
+        '''
+        # save cache
+        self.wlg.daprs[0].cache.save()
+        # logout from whatcd to avoid ghost sessions
+        for dapr in self.wlg.daprs:
+            if isinstance(dapr, whatlastgenre.dataprovider.WhatCD):
+                dapr.logout()
+                break
 
     def commands(self):
         cmds = Subcommand('wlg', help='get genres with whatlastgenre')
-        cmds.parser.add_option('-v', '--verbose', dest='verbose',
-                               action='count', default=0,
-                               help='verbose output (-vv for debug)')
-        cmds.parser.add_option('-f', '--force', dest='force',
-                               action='store_true', default=False,
-                               help='force overwrite existing genres')
-        cmds.parser.add_option('-u', '--update-cache', dest='cache',
-                               action='store_true', default=False,
-                               help='force update cache')
+        cmds.parser.add_option(
+            '-v', '--verbose', dest='verbose', action='count',
+            default=0, help='verbose output (-vv for debug)')
+        cmds.parser.add_option(
+            '-f', '--force', dest='force', action='store_true',
+            default=False, help='force overwrite existing genres')
+        cmds.parser.add_option(
+            '-u', '--update-cache', dest='cache', action='store_true',
+            default=False, help='force update cache')
         cmds.func = self.commanded
         return [cmds]
 
     def commanded(self, lib, opts, args):
+        '''wlg as command'''
         if not self.wlg:
-            self.lazy_setup(opts.cache, opts.verbose)
+            self.setup(opts.cache, opts.verbose)
+
         if opts.force:
             self.config['force'] = True
-        write = config['import']['write'].get(bool)
-        self.config.set_args(opts)
+
         albums = lib.albums(decargs(args))
         i = len(albums)
         try:
             for i, album in enumerate(albums, start=1):
-                print_progressbar(i, len(albums))
-                album.genre = self.genres(album)
-                album.store()
-                for item in album.items():
-                    item.genre = album.genre
-                    item.store()
-                    if write:
-                        item.try_write()
+                self._log.info(whatlastgenre.progressbar(i, len(albums)))
+                genres = self.genres(album)
+                if album.genre != genres:
+                    album.genre = genres
+                    album.store()
+                    for item in album.items():
+                        item.genre = genres
+                        item.store()
+                        if config['import']['write'].get(bool):
+                            item.try_write()
         except KeyboardInterrupt:
-            print()
-        self.wlg.print_stats(i)
+            pass
 
-    def imported(self, session, task):
+        self.wlg.print_stats(i)
+        self.setdown()
+
+    def imported(self, _, task):
+        '''wlg during import'''
         if not self.wlg:
-            self.lazy_setup()
+            self.setup()
+
         if task.is_album:
             genres = self.genres(task.album)
-            task.album.genre = genres
-            task.album.store()
-            for item in task.album.items():
-                item.genre = genres
-                item.store()
-        else:
-            genres = self.genres(task.item.get_album())
-            item.genre = genres
-            item.store()
+            if task.album.genre != genres:
+                task.album.genre = genres
+                task.album.store()
+                for item in task.album.items():
+                    item.genre = genres
+                    item.store()
 
     def genres(self, album):
-        if self.config['force'] or not album.genre:
-            metadata = whatlastgenre.Metadata(
-                path=album.item_dir(), type='beet',
-                artists=[(t.artist, t.mb_artistid) for t in album.items()],
-                albumartist=(album.albumartist, album.mb_albumartistid),
-                album=album.album, mbid_album=album.mb_albumid,
-                mbid_relgrp=album.mb_releasegroupid,
-                year=album.year, releasetype=album.albumtype)
-            genres, _ = self.wlg.query_album(metadata)
-            genres = self.config['separator'].get(unicode).join(genres)
-            self._log.info(u'genres for album {0}: {1}', album, genres)
-            return genres
-        else:
+        '''Return the current genres of an album if they exist and
+        the force option is not set or get genres from whatlastgenre.
+        '''
+        if album.genre and not self.config['force']:
             self._log.info(u'not forcing genre update for album {0}', album)
-        return album.genre
+            return album.genre
+
+        metadata = Metadata(
+            path=album.item_dir(), type='beet',
+            artists=[(t.artist, t.mb_artistid) for t in album.items()],
+            albumartist=(album.albumartist, album.mb_albumartistid),
+            album=album.album, mbid_album=album.mb_albumid,
+            mbid_relgrp=album.mb_releasegroupid,
+            year=album.year, releasetype=album.albumtype)
+
+        genres, _ = self.wlg.query_album(metadata)
+        genres = self.config['separator'].get(unicode).join(genres)
+
+        self._log.info(u'genres for album {0}: {1}', album, genres)
+        return genres
